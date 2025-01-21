ab292c9152d8c8080585d95bc863974c2897f631
karasb1 <bohdankaras456@gmail.com>
2025-01-21 23:55:48 +0200

    .

--- App1/main.cs
+++ App1/main.cs
@@ -1,10 +1,25 @@
-﻿namespace App1
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace App1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int[] array = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int multiplication = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                multiplication *= array[i];
+            }
+            Console.WriteLine($"Sum = {array.Sum()}");
+            Console.WriteLine($"Max = {array.Max()}");
+            Console.WriteLine($"Min = {array.Min()}");
+            Console.WriteLine($"Multiplication = {multiplication}");
         }   
     }
 }
