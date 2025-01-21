3f68ca449fc3de5c0435b3eb945a7580f0d24ca5
karasb1 <bohdankaras456@gmail.com>
2025-01-21 23:48:11 +0200

    .

--- App1/main.cs
+++ App1/main.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup");
         }   
     }
 }
