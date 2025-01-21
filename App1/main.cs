bf51205b10cb7252582dd7a20a89647d0eeaeeb4
karasb1 <bohdankaras456@gmail.com>
2025-01-22 00:01:11 +0200

    .

--- App1/main.cs
+++ App1/main.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            int number, result = 0;
+            number = Convert.ToInt32(Console.ReadLine());
+            while(number > 0)
+            {
+                result = result * 10 + number % 10;
+                number = number / 10;
+            }
+            Console.WriteLine(result);  
         }   
     }
 }
