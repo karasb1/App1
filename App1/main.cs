5c396577f7ae6819621454f8178160ebcf13f5fe
karasb1 <bohdankaras456@gmail.com>
2025-01-23 23:16:15 +0200

    .

--- App1/main.cs
+++ App1/main.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter line length :");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the sign :");
+            string sign = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter 1 - Horizontal; 2 - Vertical");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write(sign);
+                }
+            }
+            else if (choice == 2)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.WriteLine(sign);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice");
+            }
         }   
     }
 }
