cbc587fb84a8f86f350dc783201b975a4204e453
karasb1 <bohdankaras456@gmail.com>
2025-01-23 23:08:14 +0200

    .

--- App1/main.cs
+++ App1/main.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            int a, b, n1 = 0, n2 = 1;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " ");
+            Console.Write(n2 + " ");
+            while (true)
+            {
+                int s = n1 + n2;
+                if (s < b)
+                {
+                    Console.Write(s + " ");
+                }
+                else
+                {
+                    break;
+                }
+                n1 = n2;
+                n2 = s;
+            }
         }   
     }
 }
