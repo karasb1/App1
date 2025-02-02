f42ba91c23b53c710a00d0828cc7c841fe18fb9a
karasb1 <bohdankaras456@gmail.com>
2025-02-02 21:05:32 +0200

    .

--- App1/main.cs
+++ App1/main.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            
+            double tempurarure = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Convert from Celsius to Fahrenheit: " + (tempurarure*(9/5) + 32));
         }   
     }
 }
