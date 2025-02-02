814d727190d4bd0f7d15f9d6c952b3ebbda752a3
karasb1 <bohdankaras456@gmail.com>
2025-02-02 20:54:57 +0200

    .

--- App1/main.cs
+++ App1/main.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            
+            double number1 = Convert.ToInt32(Console.ReadLine());
+            double number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number1 / 100 * number2);
         }   
     }
 }
