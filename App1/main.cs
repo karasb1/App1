a93d9615839e5f4036f2050a2d5bb7b4d88aab8c
karasb1 <bohdankaras456@gmail.com>
2025-02-02 11:32:44 +0200

    .

--- App1/main.cs
+++ App1/main.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
         }   
     }
 }
